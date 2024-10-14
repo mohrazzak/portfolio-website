1790c7a239133fadddfffbc41f33242316b8bc0c
mohrazzak <86966092+mohrazzak@users.noreply.github.com>
2024-10-14 14:33:36 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.ts
@@ -0,0 +1,34 @@
+import type { NextConfig } from 'next';
+import createMDX from '@next/mdx';
+import postgres from 'postgres';
+
+export const sql = postgres(process.env.POSTGRES_URL!, {
+  ssl: 'allow',
+});
+
+const nextConfig: NextConfig = {
+  pageExtensions: ['mdx', 'ts', 'tsx'],
+  async redirects() {
+    if (!process.env.POSTGRES_URL) {
+      return [];
+    }
+
+    let redirects = await sql`
+      SELECT source, destination, permanent
+      FROM redirects;
+    `;
+
+    return redirects.map(({ source, destination, permanent }) => ({
+      source,
+      destination,
+      permanent: !!permanent,
+    }));
+  },
+  experimental: {
+    mdxRs: true,
+  },
+};
+
+const withMDX = createMDX({});
+
+export default withMDX(nextConfig);
